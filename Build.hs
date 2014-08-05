85a83c11373f745b1c4848ee6d5d89ba363dc507
Justin Handville <nanolith@gmail.com>
2014-08-05 18:47:36 -0400

    Added support for unit testing.

--- Build.hs
+++ Build.hs
@@ -12,6 +12,7 @@ buildCxxObject compileFlags out = do
 
 main :: IO ()
 main = shakeArgs shakeOptions{shakeFiles="_build/"} $ do
+    want ["_build/test/coverage-report/index.html"]
     want ["_build/release/examples/Observer/observer_example" <.> exe]
 
     let gtestDir = ".." </> "gtest"
@@ -37,3 +38,22 @@ main = shakeArgs shakeOptions{shakeFiles="_build/"} $ do
         let os = ["_build" </> "release" </> c -<.> "o" | c <- cs]
         need os
         cmd "clang++" checkedCxxflags ldflags "-o" [out] os examplelink
+
+    --test targets
+
+    let testlink = "-lstdc++ " ++ (gtestDir </> "src" </> "gtest-all.cc")
+
+    "_build/test/coverage-report/index.html" *> \out -> do
+        need ["_build/test/patterntest" <.> exe]
+
+        () <- cmd "mkdir -p _build/test/coverage-report"
+        () <- cmd "lcov -no-external -b . --directory _build --zerocounters"
+        () <- cmd "_build/test/patterntest"
+        () <- cmd "lcov -no-external -b . --directory _build --capture --output-file _build/test/coverage-report/pattern.info"
+        cmd "genhtml --no-branch-coverage -o _build/test/coverage-report _build/test/coverage-report/pattern.info"
+
+    "_build/test/patterntest" <.> exe *> \out -> do
+        cs <- getDirectoryFiles "" ["test//*.cpp"]
+        let os = ["_build" </> "checked" </> c -<.> "o" | c <- cs]
+        need os
+        cmd "clang++" checkedCxxflags ldflags "-o" [out] os testlink
